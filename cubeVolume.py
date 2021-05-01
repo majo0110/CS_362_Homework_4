495485eb492eca983a28396971c03306358e191d
Jonathan <majo@oregonstate.edu>
2021-05-01 15:57:06 -0700

    Created function for calculating cube volume

---
+++ cubeVolume.py
@@ -0,0 +1,9 @@
+def calcCubeVol(length, width, height):
+    return (length * width * height)
+
+def main():
+    temp = calcCubeVol(3,6,10)
+    print(temp)
+
+if __name__ == '__main__':
+    main()
