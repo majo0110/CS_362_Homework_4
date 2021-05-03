43938b2504d3d9ce2044cf9c9be23a8407796dd9
Jonathan <majo@oregonstate.edu>
2021-05-02 22:19:42 -0700

    Error handling

--- avgOfList/avgList.py
+++ avgOfList/avgList.py
@@ -1,8 +1,11 @@
 def calcAvg(arr):
-    totalNum = 0
-    for i in range (len(arr)):
-        totalNum += arr[i]
-    return float(totalNum) / float(len(arr))
+    if (len(arr) != 0):
+        totalNum = 0
+        for i in range (len(arr)):
+            totalNum += arr[i]
+        return float(totalNum) / float(len(arr))
+    else:
+        return 0
 
 if __name__ == '__main__':
     main()
