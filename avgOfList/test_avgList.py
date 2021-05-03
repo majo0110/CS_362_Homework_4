1bf3056b2fb9279f1d4860f008508fd181788e02
Jonathan <majo@oregonstate.edu>
2021-05-02 22:19:17 -0700

    Added 3 test cases for the test module. 1. test an empty list. 2. test a list of zeros 3. Test a list od strings

--- avgOfList/test_avgList.py
+++ avgOfList/test_avgList.py
@@ -6,5 +6,17 @@ class testCubeVolume(unittest.TestCase):
         tArray1 = [0, 15, 30, 100, 27]
         self.assertEqual(avgList.calcAvg(tArray1), 34.4)
 
+    def test_emptyList(self):
+        tArray1 = []
+        self.assertEqual(avgList.calcAvg(tArray1), 0)
+
+    def test_zeroList(self):
+        tArray1 = [0,0,0,0]
+        self.assertEqual(avgList.calcAvg(tArray1), 0)
+
+    def test_string(self):
+        tArray1 = ["1", "2", "3", "4"]
+        self.assertRaises(avgList.calcAvg(tArray1), "F")
+
 if __name__ == '__main__':
     unittest.main()
