f82b6a7a927ffc730118b15071695b44867eaf06
Jonathan <majo@oregonstate.edu>
2021-05-02 21:39:01 -0700

    Added test for string and one for complex numbers

--- cubeVolume/test_cubeVolume.py
+++ cubeVolume/test_cubeVolume.py
@@ -1,9 +1,19 @@
 import unittest
 import cubeVolume
+import cmath
 
 class testCubeVolume(unittest.TestCase):
     def testRegNum(self):
         self.assertEqual(cubeVolume.calcVol(3, 15, 20), 900)
+
+    def test_negNum(self):
+        self.assertEqual(cubeVolume.calcVol(-27, 15, 10), -4050)
+
+    def test_string(self):
+        self.assertRaises(cubeVolume.calcVol("5", "10", "15"), "F")
+
+    def test_complexNum(self):
+        self.assertEqual(cubeVolume.calcVol(complex(10,2), 5, 10), (500 + 100j))
 
 if __name__ == '__main__':
     unittest.main()
