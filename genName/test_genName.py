4d4df73ff63e2ee59c28112225cfd38a90d0e830
Jonathan <majo@oregonstate.edu>
2021-05-02 23:47:46 -0700

    Added test case for regular name, testing multple name inputs for first name, and inputting integers for parameters

--- genName/test_genName.py
+++ genName/test_genName.py
@@ -5,5 +5,12 @@ class testCubeVolume(unittest.TestCase):
     def test_RegName(self):
         self.assertEqual(genName.genFullName("bobby", "lee"), "bobby lee")
 
+    def test_threeNames(self):
+        self.assertEqual(genName.genFullName("Mcdonald Middle name", "jenkins"), "Mcdonald Middle name jenkins")
+
+    def test_integers(self):
+        self.assertEqual(genName.genFullName(200, 3004), 3204)
+
+
 if __name__ == '__main__':
     unittest.main()
